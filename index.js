6a3189783af03deaf587a189bfa36fe11a098c21
Bipin-chy <chaudharibipin121@gmail.com>
2025-10-05 18:54:59 +1100

    functional library project

---
+++ index.js
@@ -0,0 +1,77 @@
+// index.js
+
+function myEach(collection, callback) {
+    const values = Array.isArray(collection) ? collection : Object.values(collection);
+    for (let i = 0; i < values.length; i++) {
+      callback(values[i]);
+    }
+    return collection;
+  }
+  
+  function myMap(collection, callback) {
+    const values = Array.isArray(collection) ? collection : Object.values(collection);
+    const newArr = [];
+    for (let i = 0; i < values.length; i++) {
+      newArr.push(callback(values[i]));
+    }
+    return newArr;
+  }
+  
+  function myReduce(collection, callback, acc) {
+    const values = Array.isArray(collection) ? collection : Object.values(collection);
+    let startIdx = 0;
+  
+    if (acc === undefined) {
+      acc = values[0];
+      startIdx = 1;
+    }
+  
+    for (let i = startIdx; i < values.length; i++) {
+      acc = callback(acc, values[i], collection);
+    }
+  
+    return acc;
+  }
+  
+  function myFind(collection, predicate) {
+    const values = Array.isArray(collection) ? collection : Object.values(collection);
+    for (let i = 0; i < values.length; i++) {
+      if (predicate(values[i])) return values[i];
+    }
+    return undefined;
+  }
+  
+  function myFilter(collection, predicate) {
+    const values = Array.isArray(collection) ? collection : Object.values(collection);
+    const filtered = [];
+    for (let i = 0; i < values.length; i++) {
+      if (predicate(values[i])) filtered.push(values[i]);
+    }
+    return filtered;
+  }
+  
+  function mySize(collection) {
+    return Array.isArray(collection)
+      ? collection.length
+      : Object.keys(collection).length;
+  }
+  
+  function myFirst(array, n = 1) {
+    if (n === 1) return array[0];
+    return array.slice(0, n);
+  }
+  
+  function myLast(array, n = 1) {
+    if (n === 1) return array[array.length - 1];
+    return array.slice(-n);
+  }
+  
+  function myKeys(object) {
+    return Object.keys(object);
+  }
+  
+  function myValues(object) {
+    return Object.values(object);
+  }
+  
+  
